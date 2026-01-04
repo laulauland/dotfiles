c6eda70ee6d2582ed8f204573f89f993784de84a
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2026-01-04 21:01:38 +0200

    fix: switch notify.fish from OSC 9 to OSC 777 for better tmux compatibility

--- shared/.config/fish/functions/notify.fish
+++ shared/.config/fish/functions/notify.fish
@@ -1,7 +1,19 @@
-function notify --description "Send a terminal notification (OSC 9)"
-    set -l message (string join " " $argv)
-    if test -z "$message"
-        set message "Done"
+function notify --description "Send a terminal notification (OSC 9/777)"
+    set -l title "Terminal"
+    set -l body (string join " " $argv)
+    
+    if test -z "$body"
+        set body "Done"
     end
-    printf '\e]9;%s\a' "$message"
+    
+    # Use OSC 777 (rxvt-style) which has explicit title;body format
+    # Format: ESC ] 777 ; notify ; title ; body ST
+    # This is more reliable than OSC 9 which can conflict with ConEmu extensions
+    
+    # Inside tmux, wrap OSC sequences in DCS passthrough
+    if set -q TMUX
+        printf '\ePtmux;\e\e]777;notify;%s;%s\a\e\\' "$title" "$body"
+    else
+        printf '\e]777;notify;%s;%s\a' "$title" "$body"
+    end
 end
