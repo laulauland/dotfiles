f7e8ad3608fb41ea237e410d9a89088fa6ac7390
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2026-01-07 14:08:32 +0000

    feat(fish): add typora function wrapper for macOS

---
+++ shared/.config/fish/functions/typora.fish
@@ -0,0 +1,3 @@
+function typora --wraps=/Applications/Typora.app/Contents/MacOS/Typora --description 'alias typora=/Applications/Typora.app/Contents/MacOS/Typora'
+    /Applications/Typora.app/Contents/MacOS/Typora $argv
+end
