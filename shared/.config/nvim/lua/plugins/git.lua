8570fcf0c800aff1ba9365b1924bb227a8b0e708
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2026-04-17 16:35:16 +0200

    nvim: add jjsigns.nvim for jj gutter signs

--- shared/.config/nvim/lua/plugins/git.lua
+++ shared/.config/nvim/lua/plugins/git.lua
@@ -1,11 +1,17 @@
 return {
-  {
-    "jceb/jiejie.nvim",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
+	{
+		"jceb/jiejie.nvim",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"evanphx/jjsigns.nvim",
+		config = function()
+			require("jjsigns").setup()
+		end
+	}
 }
