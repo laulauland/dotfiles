f71cc73c80fa7621674146f74a540e9627240ce2
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2026-05-29 16:40:56 +0200

    nvim: enable supermaven completion

--- shared/.config/nvim/lua/plugins/ai.lua
+++ shared/.config/nvim/lua/plugins/ai.lua
@@ -1,40 +1,40 @@
 return {
-	-- {
-	-- 	"supermaven-inc/supermaven-nvim",
-	-- 	config = function()
-	-- 		require("supermaven-nvim").setup({
-	-- 			log_level = "warn",
-	-- 			keymaps = {
-	-- 				accept_suggestion = "<C-y>",
-	-- 				clear_suggestion = "<C-e>",
-	-- 			},
-	-- 			-- color = {
-	-- 			-- 	suggestion_color = "#6e6a86",
-	-- 			-- 	cterm = 8,
-	-- 			-- },
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				log_level = "warn",
+				keymaps = {
+					accept_suggestion = "<C-y>",
+					clear_suggestion = "<C-e>",
+				},
+				-- color = {
+				-- 	suggestion_color = "#6e6a86",
+				-- 	cterm = 8,
+				-- },
+			})
+		end,
+	},
 
 
--- 	{
--- 		dir = "~/Code/laulauland/ne.nvim",
--- 		build = "build.lua", -- downloads model on install/update
--- 		config = function()
--- 			require("ne").setup({
--- 				keymaps = {
--- 					accept_suggestion = "<C-y>",
--- 					clear_suggestion = "<C-e>",
--- 					accept_word = "<C-Right>",
--- 					trigger_suggestion = "<C-Space>",
--- 				},
--- 				server = {
--- 					auto_start = true,
--- 				},
--- 				debug = {
--- 					enabled = true,
--- 				},
--- 			})
--- 		end,
--- 	},
+	-- 	{
+	-- 		dir = "~/Code/laulauland/ne.nvim",
+	-- 		build = "build.lua", -- downloads model on install/update
+	-- 		config = function()
+	-- 			require("ne").setup({
+	-- 				keymaps = {
+	-- 					accept_suggestion = "<C-y>",
+	-- 					clear_suggestion = "<C-e>",
+	-- 					accept_word = "<C-Right>",
+	-- 					trigger_suggestion = "<C-Space>",
+	-- 				},
+	-- 				server = {
+	-- 					auto_start = true,
+	-- 				},
+	-- 				debug = {
+	-- 					enabled = true,
+	-- 				},
+	-- 			})
+	-- 		end,
+	-- 	},
 }
